75582db8061d154485fb6746260b60d4620a991b
Michael Lobato <mike.lobato.wft@gmail.com>
2022-09-22 17:49:46 -0700

    Add new forLoop

--- friends.js
+++ friends.js
@@ -2,6 +2,12 @@ var numbers = [5, 18, 27, 38, 52, 69, 84, 99];
 
 for (var i = 0; i < numbers.length; i++) {
   if (numbers[i] > 30) {
-      console.log(numbers[i]); 
+      console.log(numbers[i]);
+  }
+}
+console.log("=====");
+for (var i = 0; i < numbers.length; i++) {
+  if (numbers[i] < 60) {
+      console.log(numbers[i]);
   }
 }
